c720dad83e9be77aa97c97c53b390ee4581c0a6b
aa <uliana.cic@gmail.com>
2025-11-17 15:44:44 +0300

    added 1 programm

---
+++ test.py
@@ -0,0 +1,9 @@
+cnt = 0
+
+with open('names.txt', 'r', encoding = 'utf-8') as file:
+    for line in file:
+        name = line.strip()
+        if name == 'Катя':
+            cnt += 1
+
+print(cnt)
